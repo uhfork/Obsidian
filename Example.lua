13b530a1f803e715ca3f57261236d904bc745277
fork <134505813+uhfork@users.noreply.github.com>
2026-08-24 22:16:02 +0700

    fix: oops 3

--- Example.lua
+++ Example.lua
@@ -1,4 +1,4 @@
-local repo = "https://raw.githubusercontent.com/uhfork/Obsidian/"
+local repo = "https://raw.githubusercontent.com/uhfork/Obsidian/main/"
 local Library = loadstring(game:HttpGet(repo .. "Library.lua"))()
 local ThemeManager = loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))()
 local SaveManager = loadstring(game:HttpGet(repo .. "addons/SaveManager.lua"))()
